73bc1ce64f02714e110bbaac27ffb97535ae3c80
Kocsis Laszlo <kocsis.laszlo@kadeno.ro>
2024-06-28 10:57:09 +0300

    Create method for checking if custom launcher is default

--- android/app/src/main/java/com/loopeliemptywithkiosk/LauncherModule.java
+++ android/app/src/main/java/com/loopeliemptywithkiosk/LauncherModule.java
@@ -2,14 +2,16 @@ package com.loopeliemptywithkiosk;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.provider.Settings;
 import android.util.Log;
-import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LauncherModule extends ReactContextBaseJavaModule {
 
@@ -25,8 +27,31 @@ public class LauncherModule extends ReactContextBaseJavaModule {
         return "LauncherModule";
     }
 
+    static boolean isMyLauncherDefault() {
+        final IntentFilter filter = new IntentFilter(Intent.ACTION_MAIN);
+        filter.addCategory(Intent.CATEGORY_HOME);
+
+        List<IntentFilter> filters = new ArrayList<IntentFilter>();
+        filters.add(filter);
+
+        final String myPackageName = reactContext.getPackageName();
+        List<ComponentName> activities = new ArrayList<ComponentName>();
+        final PackageManager packageManager = (PackageManager) reactContext.getPackageManager();
+
+        // You can use name of your package here as third argument
+        packageManager.getPreferredActivities(filters, activities, null);
+
+        for (ComponentName activity : activities) {
+            if (myPackageName.equals(activity.getPackageName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void resetPreferredLauncherAndOpenChooser() {
-        Log.d("LauncherModule", "resetPreferredLauncherAndOpenChooser");
+        boolean isDefault = isMyLauncherDefault();
+        Log.d("LauncherModule", "resetPreferredLauncherAndOpenChooser" + (isDefault ? "yes" : "no"));
         PackageManager packageManager = reactContext.getPackageManager();
         ComponentName componentName = new ComponentName(reactContext, FakeHomeActivity.class);
         packageManager.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
