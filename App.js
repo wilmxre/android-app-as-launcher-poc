aec82cd8d7d49bb05469d168341817ac8d7a3d3e
Kocsis Laszlo <kocsis.laszlo@kadeno.ro>
2024-06-28 13:44:35 +0300

    Setup foreground service in react native side

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
-import { AppState, StyleSheet, Text, View } from "react-native";
+import { AppState, StyleSheet, Text, View, Button } from "react-native";
 import RNLockTask from "react-native-lock-task";
+import ReactNativeForegroundService from "rn-foreground-service";
 
 const Stack = createNativeStackNavigator();
+
+const INTERVAL = 15 * 60 * 1000; // 15 minutes
 
 function HomeScreen() {
   useEffect(() => {
@@ -24,10 +27,45 @@ function HomeScreen() {
     addPin();
   }, []);
 
+  useEffect(() => {
+    ReactNativeForegroundService.add_task(() => log(), {
+      delay: INTERVAL,
+      onLoop: true,
+      taskId: "taskid",
+      onError: (e) => console.log(`Error logging:`, e),
+    });
+  }, []);
+
+  const startTask = () => {
+    ReactNativeForegroundService.start({
+      id: 1244,
+      title: "Foreground Service",
+      message: "We are live World",
+      icon: "ic_launcher",
+      button: true,
+      button2: true,
+      buttonText: "Button",
+      button2Text: "Anther Button",
+      buttonOnPress: "cray",
+      setOnlyAlertOnce: true,
+      color: "#000000",
+      progress: {
+        max: 100,
+        curr: 50,
+      },
+    });
+  };
+
+  const stopTask = () => {
+    ReactNativeForegroundService.stopAll();
+  };
+
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
       <StatusBar style="auto" />
+      <Button onPress={startTask} title="Start The foreground Service" />
+      <Button onPress={stopTask} title="Stop The foreground Service" />
     </View>
   );
 }
@@ -48,5 +86,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    gap: 64,
   },
 });
